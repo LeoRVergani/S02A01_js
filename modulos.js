04a9ba72dbe68edd00b91c535217a3eda654c8b1
leorvergani <vergani@outlook.com.br>
2025-01-31 10:23:18 -0300

    update

---
+++ modulos.js
@@ -0,0 +1,8 @@
+import os from "os";
+import fs from "fs";
+
+// OS = módulo com info do sistema
+console.log("Plataforma:", os.platform());
+console.log("Arquitetura:", os.arch());
+console.log("Total de Memória:", os.totalmem());
+
