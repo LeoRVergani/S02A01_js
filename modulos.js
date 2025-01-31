a9115f9f06931ea0f385f923a4ed263209b7d987
leorvergani <vergani@outlook.com.br>
2025-01-31 11:03:11 -0300

    classroom nodejs

--- modulos.js
+++ modulos.js
@@ -6,3 +6,22 @@ console.log("Plataforma:", os.platform());
 console.log("Arquitetura:", os.arch());
 console.log("Total de Memória:", os.totalmem());
 
+// FS - Módulo que interage com pastas e arquivos
+
+// write - escreve o arquivo do zero
+fs.writeFileSync("dados.txt", "Hello, World");
+
+// append = adicionar texto ao arquivo
+
+fs.appendFileSync("dados.txt", "\nHey!");
+fs.appendFileSync("dados.txt", "\nHey!");
+
+// read = Ler um arquivo existente
+let dados = fs.readFileSync("dados.txt");
+console.log(dados.toString());
+
+import cpf from "cpf";
+
+console.log(cpf.generate());
+console.log(cpf.format("99900099900"));
+
